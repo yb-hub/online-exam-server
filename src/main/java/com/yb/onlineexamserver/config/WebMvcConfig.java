8377971703c10be9792834091fc5eab8160e9bb7
yang <840823561@qq.com>
2019-11-06 20:17:50 +0800

    新增：跨域配置

---
+++ src/main/java/com/yb/onlineexamserver/config/WebMvcConfig.java
@@ -0,0 +1,24 @@
+package com.yb.onlineexamserver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Auther: Yang
+ * @Date: 2019/11/06 20:14
+ * @Description: springmvc配置类
+ */
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(false)
+                .maxAge(3600);
+    }
+}
